6b8d7af7c4e7b013e61d733814b12bfb89450c78
fonsecadh <UO258318@uniovi.es>
2019-11-12 14:46:34 +0100

    ECMAScript Exercise 2 completed.

--- ECMAScript/Ejercicio2/InfoNavegador.js
+++ ECMAScript/Ejercicio2/InfoNavegador.js
@@ -1,33 +1,62 @@
 "use strict";
 class InfoNavegador {
-    constructor(nombreCodigo, nombreNav, version, cookiesActivas, onLine, plataforma, userAgent, lenguajeNav, vendedor, producto) {
+    constructor(nombreCodigo, nombreNav, version, cookiesActivas, enLinea, plataforma, agenteUsuario, lenguajeNav, vendedor, producto) {
         this.nombreCodigo = nombreCodigo;
         this.nombreNav = nombreNav;
         this.version = version;
         this.cookiesActivas = cookiesActivas;
-        this.onLine = onLine;
+        this.enLinea = enLinea;
         this.plataforma = plataforma;
-        this.userAgent = userAgent;
+        this.agenteUsuario = agenteUsuario;
         this.lenguajeNav = lenguajeNav;
         this.vendedor = vendedor;
         this.producto = producto;
     }
     escribirNombreCodigo() {
-        document.write(this.nombreCodigo);
+        document.write("Nombre del código: " + this.nombreCodigo);
+    }
+    escribirNombreNav() {
+        document.write("Nombre del navegador: " + this.nombreNav);
+    }
+    escribirVersion() {
+        document.write("Versión: " + this.version);
+    }
+    escribirCookiesActivas() {
+        document.write("Cookies activas: " + this.cookiesActivas);
+    }
+    escribirEnLinea() {
+        document.write("En línea: " + this.enLinea);
+    }
+    escribirPlataforma() {
+        document.write("Plataforma: " + this.plataforma);
+    }
+    escribirAgenteUsuario() {
+        document.write("Agente usuario: " + this.agenteUsuario);
+    }
+    escribirLenguajeNav() {
+        document.write("Idioma navegador: " + this.lenguajeNav);
+    }
+    escribirVendedor() {
+        document.write("Vendedor: " + this.vendedor);
+    }
+    escribirProducto() {
+        document.write("Producto: " + this.producto);
     }
     escribirParrafo(textoP) {
         document.write("<p>" + textoP + "</p>");
     }
     escribirParrafos() {
-        this.escribirParrafo(this.cursoActual);
-        this.escribirParrafo(this.nombreEstudiante);
-        this.escribirParrafo(this.email);
+        this.escribirParrafo("Nombre del código: " + this.nombreCodigo);
+        this.escribirParrafo("Versión: " + this.version);
+        this.escribirParrafo("Cookies activas: " + this.cookiesActivas);
+        this.escribirParrafo("En línea: " + this.enLinea);
+        this.escribirParrafo("Plataforma: " + this.plataforma);
+        this.escribirParrafo("Agente usuario: " + this.agenteUsuario);
+        this.escribirParrafo("Vendedor: " + this.vendedor);
+        this.escribirParrafo("Producto: " + this.producto);
     }    
 }
-var cabecera = new Cabecera("Software y Estándares para la Web", 
-    "Ingeniería Informática del Software", 
-    "Escuela de Ingeniería Informática del Software",
-    "Universidad de Oviedo",
-    "Cuarto",
-    "Hugo Fonseca Díaz",
-    "UO258318@uniovi.es");
+var theBrowser = navigator
+var infoNavegador = new InfoNavegador(theBrowser.appCodeName, theBrowser.appName, theBrowser.appVersion, theBrowser.cookieEnabled, theBrowser.onLine, theBrowser.platform, theBrowser.userAgent, theBrowser.language, theBrowser.vendor, theBrowser.product);
+
+
---
+++ ECMAScript/Ejercicio2/NombreNavegador.js
@@ -0,0 +1,1 @@
+infoNavegador.escribirNombreNav();
