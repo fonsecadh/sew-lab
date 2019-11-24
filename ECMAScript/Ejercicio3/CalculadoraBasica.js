540a5109561e3eebb3f447eccf608f69107ddbc0
fonsecadh <UO258318@uniovi.es>
2019-11-24 12:20:00 +0100

    Working on the basic calculator.

--- ECMAScript/Ejercicio3/CalculadoraBasica.js
+++ ECMAScript/Ejercicio3/CalculadoraBasica.js
@@ -3,16 +3,43 @@ class BasicCalculator {
     constructor() {
         // We store the result text field into a class attribute
         this.resultTxtField = document.getElementById("resTxt");
+        this.resultTxtField.value = ""; // Restart the text field value
 
-        // We add the listeners for the buttons representing the calculator numbers
+        // Save object reference
+        var _this = this; 
+
+        // We add the listeners for the buttons representing the calculator's numbers
         var i;
         for (i = 0; i <= 9; i++) {
-            var aux = "num"; 
-            var numId = aux.concat(i.toString());
-            document.getElementById(numId).addEventListener("click", () => this.resultTxtField.value += i.toString()); 
-        }
-    }
+            var numId = "num".concat(i.toString()); // Create the button ID
 
+            // Add listeners for the number buttons
+            document.getElementById(numId).addEventListener("click", function(wrapBtnNumber) {
+                return function() { _this.resultTxtField.value += wrapBtnNumber.toString(); }
+            }(i)); // We are wrapping the listener function because we are inside a loop
+        }
+
+        // We add the listeners for the buttons representing the calculator's operators
+        document.getElementById("opDiv").addEventListener("click", function() {
+            _this.resultTxtField.value += "/";
+        });
+
+        document.getElementById("opMul").addEventListener("click", function() {
+            _this.resultTxtField.value += "*";
+        });
+        
+        document.getElementById("opMinus").addEventListener("click", function() {
+            _this.resultTxtField.value += "-";
+        });
+
+        document.getElementById("opPlus").addEventListener("click", function() {
+            _this.resultTxtField.value += "+";
+        });
+    }
 }
 
-var calc = new BasicCalculator();
+window.addEventListener("load", function() { 
+    new BasicCalculator();
+});
+
+
