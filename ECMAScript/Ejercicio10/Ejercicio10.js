17dcfeece0c90f822df8815a1939fc0d9850d689
fonsecadh <UO258318@uniovi.es>
2019-12-07 10:56:27 +0100

    Exercise 10 Finished.

--- ECMAScript/Ejercicio10/Ejercicio10.js
+++ ECMAScript/Ejercicio10/Ejercicio10.js
@@ -1,42 +1,51 @@
 "use strict";
 class CurrencyConverter {
     constructor() {
+        this.clearTextFields();
         this.loadComboBoxes();
         this.addBtnListener();
     }
+    clearTextFields() {
+        $("#inputID").val(0);
+        $("#outputID").val(0);
+    }
     loadComboBoxes() {
         $(document).ready(function () {
-            let apiKey = "";
-            // TODO: Load only five currencies (Including Euro)
-            $.getJSON("https://data.fixer.io/api/symbols?access_key=" + apiKey, function(jsonData) {
+            let apiKey = "5711617e09a0eda074eda9afd34f0c87";
+            $.getJSON("http://data.fixer.io/api/symbols?access_key=" + apiKey, function(jsonData) {
                 let d = jsonData;
                 for (var s in d.symbols) {
-                    // Load combo boxes
-                    let item = "<option value=" + d.symbols[s.toString()] + ">" + s + "</option>;
-                    $("#actual").append(item);
-                    $("#toChange").append(item);
+                    if (s === "EUR") {
+                        // Load combo boxes for the base currency
+                        let item = "<option value=" + s + ">" + d.symbols[s.toString()] + "</option>";
+                        $("#actual").append(item);
+                    } else if (s === "USD" || s === "CAD" || s === "ALL" || s === "JPY") {
+                        // Load combo boxes for the currency to be changed
+                        let item = "<option value=" + s + ">" + d.symbols[s.toString()] + "</option>";
+                        $("#toChange").append(item);
+                    }
                 }
             });
         });
     }
     addBtnListener() {
+        let _this = this;
         $(document).ready(function () {
             $("#btnConvert").click(function() {
-                $("#inputID").val(0);
-                $("#outputID").val(0);
                 let actualCurrencySymbol = $("#actual option:selected").val();
                 let toChangeCurrencySymbol = $("#toChange option:selected").val();
                 let amount = $("#inputID").val();
-                $("#outputID").val(this.convertCurrency(amount, actualCurrencySymbol, toChangeCurrencySymbol));
+                _this.convertCurrency(amount, actualCurrencySymbol, toChangeCurrencySymbol);
             });
         });
     }
     convertCurrency(amount, actual, toChange) {
         $(document).ready(function () {
-            let apiKey = "";
-            $.getJSON("https://data.fixer.io/api/latest?access_key=" + apiKey + "&from=" + actual + "&to=" + toChange + "&amount=" + amount, function(jsonData) {
+            let apiKey = "5711617e09a0eda074eda9afd34f0c87";
+            $.getJSON("http://data.fixer.io/api/latest?access_key=" + apiKey + "&base=" + actual + "&symbols=" + toChange, function(jsonData) {
                 let d = jsonData;
-                return d.result;
+                let result = amount * d.rates[toChange.toString()];
+                $("#outputID").val(result);
             });
         });
     }
