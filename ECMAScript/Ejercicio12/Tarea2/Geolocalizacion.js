4cd7cf3beffcdc4d1598d1aa8211a754cdeddaf2
fonsecadh <UO258318@uniovi.es>
2019-12-08 18:48:14 +0100

    Exercise 12. Task 2 finished.

---
+++ ECMAScript/Ejercicio12/Tarea2/Geolocalizacion.js
@@ -0,0 +1,50 @@
+"use strict";
+class Geolocalizacion {
+    constructor() {
+        this.showCoordinates();
+    }
+
+    showCoordinates() {
+        // Save object reference
+        let _this = this;
+
+        $(document).ready(function () {
+            $("#btnShowCoord").click(function() {
+                if (window.navigator.geolocation) {
+                    window.navigator.geolocation.getCurrentPosition(_this.showCurrentPosition, _this.handleError);
+                } else {
+                    alert("El navegador no soporta la geolocalización");
+                }
+            });
+        });
+    }
+
+    showCurrentPosition(position) {
+        $(document).ready(function () {
+            let data = "";
+
+            data += "<li>Latitud: " + position.coords.latitude + " grados</li>";
+            data += "<li>Longitud: " + position.coords.longitude + " grados</li>";
+            data += "<li>Precisión (Latitud/Longitud): " + position.coords.accuracy + " metros</li>";
+            if (position.coords.altitude !== null) {
+                data += "<li>Altitud: " + position.coords.altitude + " metros</li>";
+                data += "<li>Precisión (Altitud): " + position.coords.altitudeAccuracy + " metros</li>";
+            }
+
+            $("#coordPosition").after(data);
+        });
+    }
+
+    handleError(error) {
+        alert("Ha ocurrido un error: (Código de error: " + error.code + ")" + error.message); 
+    }
+}
+
+
+window.addEventListener("load", function() { 
+    new Geolocalizacion();
+});
+
+
+
+
