80ace2e3677a945b1e12374935cd9e093e117c48
fonsecadh <UO258318@uniovi.es>
2019-12-09 11:33:42 +0100

    Exercise 14. Task 1 finished.

--- ECMAScript/Ejercicio14/Tarea1/MapaKML.js
+++ ECMAScript/Ejercicio14/Tarea1/MapaKML.js
@@ -1,24 +1,10 @@
 "use strict";
 class MapKML {
-    constructor() {
-        this.addKMLBtnListener();
-        this.theMap = "";
-        this.theMarker = "";
-    }
-
     showMap() {
         let fushimiInari = { lat: 34.96676689, lng: 135.77084584 }; 
-        this.theMap = new google.maps.Map(document.getElementById("map-canvas"), { zoom: 10, center: fushimiInari });
-        this.theMarker = new google.maps.Marker({ position: fushimiInari, map: this.theMap }); 
-    }
-
-    addKMLBtnListener() {
-        $(document).ready(function () {
-            $("#btnShowKML").click(function() {
-                let kmlUrl = "https://raw.githubusercontent.com/fonsecadh/sew-lab/master/ECMAScript/Ejercicio14/rutas.kml";
-                let kmlLayer = new google.maps.KmlLayer(kmlUrl, { supressInfoWindows: true, preserveViewport: false, map: this.theMap });
-            });
-        });
+        let theMap = new google.maps.Map(document.getElementById("map-canvas"), { zoom: 7, center: fushimiInari, mapTypeId: "terrain" });
+        let kmlUrl = "https://raw.githubusercontent.com/fonsecadh/sew-lab/master/ECMAScript/Ejercicio14/Tarea1/rutas.kml";
+        let kmlLayer = new google.maps.KmlLayer(kmlUrl, { supressInfoWindows: true, preserveViewport: false, map: theMap });
     }
 }
 
